11c23b785d6a382f49f6d12e52b624910e8151da
Austin Hou <austin@austinhou.com>
2022-06-18 16:39:44 -0700

    refactor

--- index.js
+++ index.js
@@ -2,6 +2,17 @@
 exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
+var extractTodoFields = function (todo) {
+    var id = todo.id;
+    var title = todo.title;
+    var completed = todo.completed;
+    return { id: id, title: title, completed: completed };
+};
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    var todo = response.data;
+    var _a = extractTodoFields(todo), id = _a.id, title = _a.title, completed = _a.completed;
+    logTodo(id, title, completed);
 });
+var logTodo = function (id, title, completed) {
+    console.log("\n\t\tThe Todo with ID: ".concat(id, "\n\t\tHas a title of: ").concat(title, "\n\t\tIs it finished? ").concat(completed, "\n\t"));
+};
--- index.ts
+++ index.ts
@@ -3,23 +3,27 @@ import axios from 'axios';
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 interface Todo {
-	id: number;
-	title: string;
-	completed: boolean;
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+const extractTodoFields = (todo: Todo) => {
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+    return {id, title, completed};
 }
 
 axios.get(url).then((response) => {
-	const todo = response.data as Todo;
+    const todo = response.data as Todo;
+    const {id, title, completed} = extractTodoFields(todo);
 
-	const id = todo.id;
-	const title = todo.title;
-	const completed = todo.completed;
-
-	logTodo(id, title, completed);
+    logTodo(id, title, completed);
 });
 
 const logTodo = (id: number, title: string, completed: boolean) => {
-	console.log(`
+    console.log(`
 		The Todo with ID: ${id}
 		Has a title of: ${title}
 		Is it finished? ${completed}
